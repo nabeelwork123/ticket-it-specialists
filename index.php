ee03ea1dc12a05a89a40c50f18a0567a88617362
nabeelwork123 <nabeelwork123@gmail.com>
2025-04-18 21:28:34 +0500

    update

--- index.php
+++ index.php
@@ -174,7 +174,7 @@
 
 					</div>
 
-					<div class="col-xl-3 col-lg-9 col-lg-12 col-12">
+					<div class="col-xl-3 col-lg-3 col-lg-12 col-12">
 
 					
 						<div class="card flex-fill">
@@ -185,17 +185,17 @@
 								<!-- <p class="fs-12 mb-0">&nbsp; </p> -->
 								<div class="ov-box">
 									<div class="ov-box-inner">
-									<span class="fs-12 fw-medium text-dark ">24<span class="fs-10">Hrs</span></span>
+									<span class="fs-14 fw-medium text-dark ">24<span class="fs-10">Hrs</span></span>
 									<p class="fs-12  mt-3">7</p>
 									<p class="fs-12  mb-0 mt-3">tickets </p>
 									</div>
 									<div class="ov-box-inner">
-									<span class="fs-12 fw-medium text-dark">48<span class="fs-10">Hrs</span></span>
+									<span class="fs-14 fw-medium text-dark">48<span class="fs-10">Hrs</span></span>
 									<p class="fs-12  mt-3">10</p>
 									<p class="fs-12  mb-0 mt-3">tickets </p>
 									</div>
 									<div class="ov-box-inner" style="border:0;">
-									<span class="fs-12 fw-medium text-dark ">72<span class="fs-10">Hrs</span></span>
+									<span class="fs-14 fw-medium text-dark ">72<span class="fs-10">Hrs</span></span>
 									<p class="fs-12  mt-3">15</p>
 									<p class="fs-12  mb-0 mt-3">tickets </p>
 									</div>
@@ -933,19 +933,19 @@
 							<div class="card-body">
 								<div class="row">
 									<div class="col-6 be-1 bb-1 ">
-										<div class="d-flex align-items-center justify-content-between">
+										<div class="d-flex align-items-center justify-content-between mb-4 px-4 py-3">
 											<div class="w-75">
 												<p class="text-mute m-0 mb-0">Tickets picked</p>
 												<h3 class="mb-0 fs-32 fw-500 l-h-50">180 tickets</h3>
 											</div>
-											<div class="w-25 mt-3">
+											<div class="w-25 mt-3 text-end">
 												<img src="assets/img/icons/click.png" class="img-fluid">
 											</div>
 										</div>
 									</div>
 
 									<div class="col-6 be-1 bb-1 ">
-										<div class="d-flex align-items-center justify-content-between">
+										<div class="d-flex align-items-center justify-content-between bg-light p-2 rounded mb-4 px-4 py-3">
 											<div class="w-75">
 											<h3 class="mb-0 fs-32 fw-500 l-h-50">75%</h3>
 												<p class="text-mute m-0 mb-2">Positive</p>
@@ -959,7 +959,7 @@
 									</div>
 
 									<div class="col-6 be-1 bb-1 ">
-										<div class="d-flex align-items-center justify-content-between">
+										<div class="d-flex align-items-center justify-content-between bg-light p-2 rounded mb-4 px-4 py-3">
 											<div class="w-75">
 											<h3 class="mb-0 fs-32 fw-500 l-h-50">20%</h3>
 												<p class="text-mute m-0 mb-2">Neutral</p>
@@ -974,7 +974,7 @@
 
 
 									<div class="col-6 be-1 bb-1 ">
-										<div class="d-flex align-items-center justify-content-between">
+										<div class="d-flex align-items-center justify-content-between bg-light p-2 rounded mb-4 px-4 py-3">
 											<div class="w-75">
 											<h3 class="mb-0 fs-32 fw-500 l-h-50">5%</h3>
 												<p class="text-mute m-0 mb-2">Negative</p>
